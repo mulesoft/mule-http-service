2dcb084db4203049b535177acdc52b74880a649d
Rodrigo Merino <rodrigo.merino@mulesoft.com>
2017-05-23 16:12:55 -0300

    MULE-12406: HTTP, Oauth and Soap services have wrong package name

---
+++ src/test/java/org/mule/service/http/impl/service/server/grizzly/ResponseCompletionHandlerTestCase.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.service.http.impl.service.server.grizzly;
+
+import static org.mockito.Mockito.mock;
+import static org.mule.service.http.impl.service.AllureConstants.HttpFeature.HTTP_SERVICE;
+import static org.mule.service.http.impl.service.AllureConstants.HttpFeature.HttpStory.RESPONSES;
+
+import org.mule.runtime.http.api.domain.message.response.HttpResponse;
+import org.mule.service.http.impl.service.server.grizzly.BaseResponseCompletionHandler;
+import org.mule.service.http.impl.service.server.grizzly.ResponseCompletionHandler;
+
+import ru.yandex.qatools.allure.annotations.Features;
+import ru.yandex.qatools.allure.annotations.Stories;
+
+@Features(HTTP_SERVICE)
+@Stories(RESPONSES)
+public class ResponseCompletionHandlerTestCase extends BaseResponseCompletionHandlerTestCase {
+
+  private ResponseCompletionHandler handler = new ResponseCompletionHandler(ctx, request, mock(HttpResponse.class), callback);
+
+  @Override
+  protected BaseResponseCompletionHandler getHandler() {
+    return handler;
+  }
+
+}
