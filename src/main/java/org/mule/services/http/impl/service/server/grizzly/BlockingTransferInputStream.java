c550269047c6129b93fb7b3348a32584a89865db
Daniel Feist <dfeist@gmail.com>
2017-05-23 15:39:07 +0100

    MULE-12559 BlockingTransferInputStream should implement read(byte[] b, int off, int len) (#4)

--- src/main/java/org/mule/services/http/impl/service/server/grizzly/BlockingTransferInputStream.java
+++ src/main/java/org/mule/services/http/impl/service/server/grizzly/BlockingTransferInputStream.java
@@ -9,39 +9,98 @@ package org.mule.services.http.impl.service.server.grizzly;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.glassfish.grizzly.ReadResult;
 import org.glassfish.grizzly.filterchain.FilterChainContext;
-import org.glassfish.grizzly.http.HttpContent;
-import org.glassfish.grizzly.utils.BufferInputStream;
+import org.glassfish.grizzly.http.HttpHeader;
+import org.glassfish.grizzly.http.io.InputBuffer;
 
 /**
- * {@link InputStream} to be used when the HTTP request has Transfer-Encoding: chunked or the content is not fully
- * provided because the message is too large.
+ * {@link InputStream} to be used when the HTTP request has Transfer-Encoding: chunked or the content is not fully provided
+ * because the message is too large.
  *
- * This {@link InputStream} implementation does a blocking read over the HTTP connection to read the next chunk when there
- * is no more data available.
+ * This {@link InputStream} implementation does a blocking read over the HTTP connection to read the next chunk when there is no
+ * more data available.
  */
-public class BlockingTransferInputStream extends InputStream {
+final class BlockingTransferInputStream extends InputStream {
 
-  private final FilterChainContext filterChainContext;
-  private InputStream chunk;
-  private boolean lastPacketReceived;
+  private final InputBuffer inputBuffer;
 
-  public BlockingTransferInputStream(FilterChainContext filterChainContext, InputStream firstChunk) {
-    this.filterChainContext = filterChainContext;
-    this.chunk = firstChunk;
+  BlockingTransferInputStream(HttpHeader httpHeader, FilterChainContext ctx) {
+    inputBuffer = new InputBuffer();
+    inputBuffer.initialize(httpHeader, ctx);
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public int read() throws IOException {
-    int value = chunk.read();
-    if (value == -1 && !lastPacketReceived) {
-      ReadResult readResult = filterChainContext.read();
-      HttpContent httpContent = (HttpContent) readResult.getMessage();
-      lastPacketReceived = httpContent.isLast();
-      chunk = new BufferInputStream(httpContent.getContent());
-      value = chunk.read();
-    }
-    return value;
+    return inputBuffer.readByte();
   }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public int read(byte[] b) throws IOException {
+    return inputBuffer.read(b, 0, b.length);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public int read(byte[] b, int off, int len) throws IOException {
+    return inputBuffer.read(b, off, len);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public long skip(long n) throws IOException {
+    return inputBuffer.skip(n);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public int available() throws IOException {
+    return inputBuffer.available();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void close() throws IOException {
+    inputBuffer.close();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void mark(int readlimit) {
+    inputBuffer.mark(readlimit);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void reset() throws IOException {
+    inputBuffer.reset();
+  }
+
+  /**
+   * This {@link InputStream} implementation supports marking.
+   *
+   * @return <code>true</code>
+   */
+  @Override
+  public boolean markSupported() {
+    return inputBuffer.markSupported();
+  }
+
 }
