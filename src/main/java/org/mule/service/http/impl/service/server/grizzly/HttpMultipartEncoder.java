d9ede97412f60dd81a5335ac47cc36884609cf0d
Facundo Velazquez <facundovelazquezsantillan@gmail.com>
2017-07-06 15:00:20 -0300

    MULE-12973: Add support for start parameter in http multipart/related response (#16)
    According to this RFC article: https://tools.ietf.org/html/rfc2387, in multipart/related content type: The start parameter, if given, is the content-ID
    of the compound object's "root". If not present the "root" is the first body part in the Multipart/Related entity. Additionally, we shoud also honor
    the following restrictions:
    
    - Type parameter is mandatory in multipart/related.
    - The value of the content-type parameter must be the same that the root body part's content-type.

--- src/main/java/org/mule/service/http/impl/service/server/grizzly/HttpMultipartEncoder.java
+++ src/main/java/org/mule/service/http/impl/service/server/grizzly/HttpMultipartEncoder.java
@@ -6,7 +6,9 @@
  */
 package org.mule.service.http.impl.service.server.grizzly;
 
+import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
 import static org.mule.runtime.http.api.HttpHeaders.Names.CONTENT_DISPOSITION;
+import static org.mule.runtime.http.api.HttpHeaders.Names.CONTENT_ID;
 import static org.mule.runtime.http.api.HttpHeaders.Names.CONTENT_TYPE;
 import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.core.api.util.IOUtils;
@@ -33,9 +35,23 @@ public class HttpMultipartEncoder {
 
   private static final String FORM_DATA = "form-data";
   public static final String ATTACHMENT = "attachment";
+  private static final String RELATED = "related";
+  private static final String TYPE_PARAMETER = "type";
+  private static final String START_PARAMETER = "start";
+  public static final String MANDATORY_TYPE_ERROR_MESSAGE =
+      "Type parameter is not present in multipart/related content type, but it is mandatory.";
+  public static final String AMBIGUOUS_TYPE_ERROR_MESSAGE = "Type parameter and root body part content type must be the same.";
+
+  private HttpMultipartEncoder() {}
 
   public static MimeMultipart toMimeMultipart(HttpEntity body, String contentType) throws IOException {
     String contentTypeSubType = getContentTypeSubType(contentType);
+    String typeParameter = getContentTypeParameter(contentType, TYPE_PARAMETER);
+
+    if (contentTypeSubType.equals(RELATED) && typeParameter == null) {
+      throw new MuleRuntimeException(createStaticMessage(MANDATORY_TYPE_ERROR_MESSAGE));
+    }
+
     MimeMultipart mimeMultipartContent = new HttpMimeMultipart(contentType, contentTypeSubType);
     final Collection<HttpPart> parts = body.getParts();
 
@@ -51,17 +67,40 @@ public class HttpMultipartEncoder {
         String partType = contentTypeSubType.equals(FORM_DATA) ? FORM_DATA : ATTACHMENT;
         internetHeaders.addHeader(CONTENT_DISPOSITION, getContentDisposition(part, partType));
       }
+      if (contentTypeSubType.equals(RELATED) && part.getName() != null) {
+        internetHeaders.addHeader(CONTENT_ID, part.getName());
+      }
       if (internetHeaders.getHeader(CONTENT_TYPE) == null && part.getContentType() != null) {
         internetHeaders.addHeader(CONTENT_TYPE, part.getContentType());
       }
       try {
         // TODO: MULE-12827 - Support HTTP multipart streaming
         final byte[] partContent = IOUtils.toByteArray(part.getInputStream());
-        mimeMultipartContent.addBodyPart(new MimeBodyPart(internetHeaders, partContent));
+
+        String rootContentId = getContentTypeParameter(contentType, START_PARAMETER);
+
+        if (contentTypeSubType.equals(RELATED) && part.getName() != null && part.getName().equals(rootContentId)) {
+          mimeMultipartContent.addBodyPart(new MimeBodyPart(internetHeaders, partContent), 0);
+        } else {
+          mimeMultipartContent.addBodyPart(new MimeBodyPart(internetHeaders, partContent));
+        }
+
       } catch (Exception e) {
-        throw new RuntimeException(e);
+        throw new MuleRuntimeException(e);
       }
     }
+
+    try {
+      if (contentTypeSubType.equals(RELATED) && mimeMultipartContent.getCount() > 0) {
+        String rootBodyPartContentType = mimeMultipartContent.getBodyPart(0).getContentType();
+        if (rootBodyPartContentType != null && (!rootBodyPartContentType.equals(typeParameter))) {
+          throw new MuleRuntimeException(createStaticMessage(AMBIGUOUS_TYPE_ERROR_MESSAGE));
+        }
+      }
+    } catch (MessagingException e) {
+      throw new MuleRuntimeException(e);
+    }
+
     return mimeMultipartContent;
   }
 
@@ -106,4 +145,21 @@ public class HttpMultipartEncoder {
     }
     return builder.toString();
   }
+
+  /**
+   * Extracts a parameter value from a content type
+   *
+   * @param contentType the content type
+   * @param parameterName: the name of the parameter to extract.
+   * @return the value of the extracted parameter.
+   */
+  public static String getContentTypeParameter(String contentType, String parameterName) {
+    try {
+      ContentType contentTypeValue = new ContentType(contentType);
+      return contentTypeValue.getParameter(parameterName);
+    } catch (ParseException e) {
+      throw new MuleRuntimeException(e);
+    }
+  }
+
 }
