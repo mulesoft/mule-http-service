567da5f8bc84891748e128448b52a349077cb363
Rodro <rodrigo.merino@mulesoft.com>
2017-11-08 09:09:36 -0300

    test case that reproduces

---
+++ src/test/java/org/mule/service/http/impl/functional/server/HttpServerClientTimeoutTestCase.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.service.http.impl.functional.server;
+
+import static java.lang.String.format;
+import static java.util.Collections.singletonList;
+import static org.junit.Assert.fail;
+import static org.mule.runtime.api.metadata.MediaType.TEXT;
+import static org.mule.runtime.http.api.HttpConstants.Method.GET;
+import static org.mule.runtime.http.api.HttpHeaders.Names.CONTENT_TYPE;
+
+import org.mule.runtime.http.api.domain.entity.ByteArrayHttpEntity;
+import org.mule.runtime.http.api.domain.message.response.HttpResponse;
+import org.mule.runtime.http.api.server.HttpServer;
+import org.mule.runtime.http.api.server.HttpServerConfiguration;
+import org.mule.runtime.http.api.server.async.ResponseStatusCallback;
+import org.mule.service.http.impl.functional.AbstractHttpServiceTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+
+import org.apache.http.client.fluent.Request;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+
+public class HttpServerClientTimeoutTestCase extends AbstractHttpServiceTestCase {
+
+  @Rule
+  public DynamicPort port = new DynamicPort("port");
+
+  private HttpServer server;
+
+  private CountDownLatch requestLatch;
+  private CountDownLatch responseLatch;
+
+  public HttpServerClientTimeoutTestCase(String serviceToLoad) {
+    super(serviceToLoad);
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    requestLatch = new CountDownLatch(1);
+    responseLatch = new CountDownLatch(1);
+
+    server = service.getServerFactory().create(new HttpServerConfiguration.Builder()
+        .setHost("localhost")
+        .setPort(port.getNumber())
+        .setName("client-timeout-test")
+        .build());
+    server.start();
+    server.addRequestHandler(singletonList(GET.name()), "/test", (requestContext, responseCallback) -> {
+
+      try {
+        requestLatch.await();
+      } catch (InterruptedException e) {
+        // Nothing to do
+      }
+
+      System.out.println("Continue processing after client timeout");
+
+      responseCallback.responseReady(HttpResponse.builder().statusCode(500).entity(new ByteArrayHttpEntity("Success!".getBytes()))
+          .addHeader(CONTENT_TYPE, TEXT.toRfcString())
+          .build(), new ResponseStatusCallback() {
+
+            @Override
+            public void responseSendFailure(Throwable throwable) {
+              throwable.printStackTrace();
+            }
+
+            @Override
+            public void responseSendSuccessfully() {
+              System.out.println("Success?");
+            }
+
+          });
+
+      responseLatch.countDown();
+    });
+  }
+
+  @After
+  public void tearDown() {
+    if (server != null) {
+      server.stop();
+    }
+  }
+
+  @Test
+  public void it() throws IOException, InterruptedException {
+    try {
+      Request.Get(format("http://localhost:%s/%s", port.getValue(), "test"))
+          .connectTimeout(5000).socketTimeout(5000).execute();
+      fail();
+    } catch (SocketTimeoutException ste) {
+      // Expected
+    }
+    server.stop();
+
+    // Calling dispose causes the CCE
+    server.dispose();
+    server = null;
+    requestLatch.countDown();
+    responseLatch.await();
+
+  }
+
+  private URI getUri(String path) {
+    return URI.create(format("http://localhost:%s/%s", port.getValue(), path));
+  }
+
+}
