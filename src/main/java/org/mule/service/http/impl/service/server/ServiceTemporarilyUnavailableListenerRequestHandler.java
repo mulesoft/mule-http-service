8199181161b96c202a9f02ccce00740db3339698
Ana Felisatti <afelisatti@mulesoft.com>
2018-03-23 21:32:54 -0300

    MULE-13034: Escape all error handlers (#87)
    (cherry picked from commit d69a40eb4786bc64dd07f09e42af1ac350aa10c4)

--- src/main/java/org/mule/service/http/impl/service/server/ServiceTemporarilyUnavailableListenerRequestHandler.java
+++ src/main/java/org/mule/service/http/impl/service/server/ServiceTemporarilyUnavailableListenerRequestHandler.java
@@ -13,12 +13,12 @@ import static org.mule.runtime.http.api.HttpConstants.HttpStatus.SERVICE_UNAVAIL
  */
 public class ServiceTemporarilyUnavailableListenerRequestHandler extends ErrorRequestHandler {
 
+  public static final String SERVICE_NOT_AVAILABLE_FORMAT = "Service not available for endpoint: %s";
   private static ServiceTemporarilyUnavailableListenerRequestHandler instance =
       new ServiceTemporarilyUnavailableListenerRequestHandler();
 
   private ServiceTemporarilyUnavailableListenerRequestHandler() {
-    super(SERVICE_UNAVAILABLE.getStatusCode(), SERVICE_UNAVAILABLE.getReasonPhrase(),
-          "Service not available for request uri: %s");
+    super(SERVICE_UNAVAILABLE.getStatusCode(), SERVICE_UNAVAILABLE.getReasonPhrase(), SERVICE_NOT_AVAILABLE_FORMAT);
   }
 
   public static ServiceTemporarilyUnavailableListenerRequestHandler getInstance() {
