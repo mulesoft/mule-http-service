f9ffe7aa27650213b3ef7be2d1b68589fb681b8f
afelisatti <afelisatti@mulesoft.com>
2018-03-29 00:11:46 -0300

    MULE-14760: HTTP request connection limit reached error is not descriptive

--- src/main/java/org/mule/service/http/impl/service/client/async/ResponseAsyncHandler.java
+++ src/main/java/org/mule/service/http/impl/service/client/async/ResponseAsyncHandler.java
@@ -9,15 +9,15 @@ package org.mule.service.http.impl.service.client.async;
 import org.mule.runtime.http.api.domain.message.response.HttpResponse;
 import org.mule.service.http.impl.service.client.HttpResponseCreator;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.ning.http.client.AsyncCompletionHandler;
 import com.ning.http.client.Response;
 
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeoutException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Non blocking {@link com.ning.http.client.AsyncHandler} which waits to load the whole response to memory before propagating it.
@@ -54,7 +54,7 @@ public class ResponseAsyncHandler extends AsyncCompletionHandler<Response> {
     } else if (t instanceof IOException) {
       exception = (IOException) t;
     } else {
-      exception = new IOException(t);
+      exception = new IOException(t.getMessage(), t);
     }
     future.completeExceptionally(exception);
   }
