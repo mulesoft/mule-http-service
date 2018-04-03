bdd786f6547da20df7d799a31058c147d84dd5f7
Ana Felisatti <afelisatti@mulesoft.com>
2018-04-03 11:52:15 -0300

    MULE-14760: HTTP request connection limit reached error is not descriptive (#88)
    (cherry picked from commit 131d80396970a7c7f03947ee794a87d2c47132d2)

---
+++ src/test/java/org/mule/service/http/impl/functional/client/HttpClientMaxConnectionsTestCase.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.service.http.impl.functional.client;
+
+import static org.hamcrest.Matchers.instanceOf;
+import org.mule.runtime.api.lifecycle.CreateException;
+import org.mule.runtime.api.util.concurrent.Latch;
+import org.mule.runtime.http.api.client.HttpClient;
+import org.mule.runtime.http.api.client.HttpClientConfiguration;
+import org.mule.runtime.http.api.domain.message.request.HttpRequest;
+import org.mule.runtime.http.api.domain.message.response.HttpResponse;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class HttpClientMaxConnectionsTestCase extends AbstractHttpClientTestCase {
+
+  private static final String ERROR_MESSAGE = "Connection limit exceeded, cannot process request";
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  private Latch handling = new Latch();
+  private Latch complete = new Latch();
+  private HttpClient client;
+
+  public HttpClientMaxConnectionsTestCase(String serviceToLoad) {
+    super(serviceToLoad);
+  }
+
+  @Before
+  public void createClient() throws CreateException {
+    client = service.getClientFactory().create(new HttpClientConfiguration.Builder()
+        .setMaxConnections(1)
+        .setName("max-connections-test")
+        .build());
+    client.start();
+  }
+
+  @After
+  public void stopClient() {
+    if (client != null) {
+      client.stop();
+    }
+  }
+
+  @Override
+  protected HttpResponse setUpHttpResponse(HttpRequest request) {
+    handling.release();
+    try {
+      complete.await();
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
+    return HttpResponse.builder().build();
+  }
+
+  @Test
+  public void failsOnConnectionsExceededWhenSync() throws Exception {
+    try {
+      client.sendAsync(getRequest(), TIMEOUT, true, null);
+      handling.await();
+      expectedException.expect(IOException.class);
+      expectedException.expectMessage(ERROR_MESSAGE);
+      client.send(getRequest(), 1, true, null);
+    } finally {
+      complete.release();
+    }
+  }
+
+  @Test
+  public void failsOnConnectionsExceededWhenAsync() throws Exception {
+    try {
+      client.sendAsync(getRequest(), TIMEOUT, true, null);
+      handling.await();
+      expectedException.expect(ExecutionException.class);
+      expectedException.expectCause(instanceOf(IOException.class));
+      expectedException.expectMessage(ERROR_MESSAGE);
+      client.sendAsync(getRequest(), 1, true, null).get();
+    } finally {
+      complete.release();
+    }
+  }
+
+  private HttpRequest getRequest() {
+    return HttpRequest.builder().uri(getUri()).build();
+  }
+
+}
