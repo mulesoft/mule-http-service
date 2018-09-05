f01aa5ea7e2b8cc320f9734477db916d1af971f0
afelisatti <afelisatti@mulesoft.com>
2018-09-05 15:16:44 -0300

    MULE-14148: Fix format

--- src/main/java/org/mule/service/http/impl/service/server/grizzly/GrizzlyServerManager.java
+++ src/main/java/org/mule/service/http/impl/service/server/grizzly/GrizzlyServerManager.java
@@ -210,7 +210,8 @@ public class GrizzlyServerManager implements HttpServerManager {
     sslFilterDelegate.addFilterForAddress(serverAddress, createSslFilter(tlsContextFactory));
     httpServerFilterDelegate
         .addFilterForAddress(serverAddress,
-                             createHttpServerFilter(connectionIdleTimeout, usePersistentConnections, delayedExecutor, identifier));
+                             createHttpServerFilter(connectionIdleTimeout, usePersistentConnections, delayedExecutor,
+                                                    identifier));
     final GrizzlyHttpServer grizzlyServer = new ManagedGrizzlyHttpServer(serverAddress, transport, httpListenerRegistry,
                                                                          schedulerSupplier,
                                                                          () -> executorProvider.removeExecutor(serverAddress),
@@ -234,7 +235,8 @@ public class GrizzlyServerManager implements HttpServerManager {
     addTimeoutFilter(serverAddress, usePersistentConnections, connectionIdleTimeout, delayedExecutor);
     httpServerFilterDelegate
         .addFilterForAddress(serverAddress,
-                             createHttpServerFilter(connectionIdleTimeout, usePersistentConnections, delayedExecutor, identifier));
+                             createHttpServerFilter(connectionIdleTimeout, usePersistentConnections, delayedExecutor,
+                                                    identifier));
     final GrizzlyHttpServer grizzlyServer = new ManagedGrizzlyHttpServer(serverAddress, transport, httpListenerRegistry,
                                                                          schedulerSupplier,
                                                                          () -> executorProvider.removeExecutor(serverAddress),
@@ -286,7 +288,8 @@ public class GrizzlyServerManager implements HttpServerManager {
     }
     HttpServerFilter httpServerFilter =
         new HttpServerFilter(true, retrieveMaximumHeaderSectionSize(), ka, delayedExecutor);
-    httpServerFilter.getMonitoringConfig().addProbes(new HttpMessageLogger(LISTENER, identifier.getName(), currentThread().getContextClassLoader()));
+    httpServerFilter.getMonitoringConfig()
+        .addProbes(new HttpMessageLogger(LISTENER, identifier.getName(), currentThread().getContextClassLoader()));
     httpServerFilter.setAllowPayloadForUndefinedHttpMethods(true);
     return httpServerFilter;
   }
