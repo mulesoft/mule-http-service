9d40e127733819ae8eadb8294cfde1ade50ad448
Maria Mariotti <mmariotti@mmariotti-ltm.internal.salesforce.com>
2019-01-23 11:54:04 -0300

    MULE-16308: Added test to validate that on multiple authentication methods supported, the one locally configured is effectively used for authentication handshake.

--- src/test/java/org/mule/service/http/impl/service/server/grizzly/HttpRequestMultipleAuthenticationMethodsSupportedTestCase.java
+++ src/test/java/org/mule/service/http/impl/service/server/grizzly/HttpRequestMultipleAuthenticationMethodsSupportedTestCase.java
@@ -1,59 +1,162 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.service.http.impl.service.server.grizzly;
 
-import static org.junit.Assert.assertFalse;
-import static org.mockito.Mockito.mock;
+import static sun.net.www.protocol.http.AuthScheme.NTLM;
+import static org.mule.runtime.http.api.HttpConstants.HttpStatus.UNAUTHORIZED;
 import static org.mule.runtime.http.api.HttpConstants.HttpStatus.OK;
+import static org.mule.runtime.http.api.HttpConstants.Method.GET;
+import static org.apache.http.HttpHeaders.WWW_AUTHENTICATE;
+import static org.apache.http.HttpHeaders.AUTHORIZATION;
 import static org.mule.service.http.impl.AllureConstants.HttpFeature.HTTP_SERVICE;
 import static org.mule.service.http.impl.AllureConstants.HttpFeature.HttpStory.SERVER_MANAGEMENT;
-import static java.util.concurrent.TimeUnit.SECONDS;
 
-import org.glassfish.grizzly.http.HttpRequestPacket;
+import org.junit.Before;
+import org.mule.runtime.api.util.Reference;
+import org.mule.runtime.core.api.util.StringUtils;
+import org.mule.runtime.http.api.client.HttpClient;
+import org.mule.runtime.http.api.client.HttpClientConfiguration;
+import org.mule.runtime.http.api.client.HttpRequestOptions;
+import org.mule.runtime.http.api.client.auth.HttpAuthentication;
+import org.mule.runtime.http.api.domain.message.request.HttpRequest;
 import org.mule.runtime.http.api.domain.message.response.HttpResponse;
-import org.mule.runtime.http.api.server.HttpServer;
-import org.mule.runtime.http.api.server.ServerAddress;
-import org.mule.runtime.http.api.server.ServerCreationException;
-import org.mule.runtime.http.api.server.async.ResponseStatusCallback;
-import org.mule.service.http.impl.service.server.DefaultServerAddress;
-import org.mule.service.http.impl.service.server.ServerIdentifier;
+import org.mule.runtime.http.api.domain.message.response.HttpResponseBuilder;
+import org.mule.service.http.impl.functional.ResponseReceivedProbe;
+import org.mule.service.http.impl.functional.client.AbstractHttpClientTestCase;
 
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.junit.Test;
-
-import java.net.InetAddress;
+import org.mule.tck.probe.PollingProber;
+import org.mule.tck.probe.Probe;
 
 @Feature(HTTP_SERVICE)
 @Story(SERVER_MANAGEMENT)
-public class HttpRequestMultipleAuthenticationMethodsSupportedTestCase extends AbstractGrizzlyServerManagerTestCase {
+public class HttpRequestMultipleAuthenticationMethodsSupportedTestCase extends AbstractHttpClientTestCase {
 
-    @Override
-    protected HttpServer getServer(ServerAddress address, ServerIdentifier id) throws ServerCreationException {
-        HttpServer server = serverManager.createServerFor(address, () -> muleContext.getSchedulerService().ioScheduler(), true,
-                (int) SECONDS.toMillis(DEFAULT_TEST_TIMEOUT_SECS), id);
+  private static final int TIMEOUT_MILLIS = 1000;
+  private static final int POLL_DELAY_MILLIS = 200;
+  private static final String DIGEST = "Digest";
+  private static final String NONCE =
+      "+Upgraded+v1a574e295ff1f41c52582b82815bb734c5c50331c97c4d301bc97f789c5e9e73ca9564b24cbd898ce5f1c13598999faa2ab013ee5b1597087";
+  private static boolean choseDigest = false;
+  private static boolean choseNtlm = false;
 
-        final ResponseStatusCallback responseStatusCallback = mock(ResponseStatusCallback.class);
+  private HttpClientConfiguration.Builder clientBuilder = new HttpClientConfiguration.Builder().setName("auth-test");
+  private PollingProber pollingProber = new PollingProber(TIMEOUT_MILLIS, POLL_DELAY_MILLIS);
 
-        server.addRequestHandler("/*", ((requestContext, responseCallback) -> {
-            responseCallback.responseReady(HttpResponse.builder().statusCode(OK.getStatusCode()).build(),
-                    responseStatusCallback);
-        }) );
+  public HttpRequestMultipleAuthenticationMethodsSupportedTestCase(String serviceToLoad) {
+    super(serviceToLoad);
+  }
 
-        return server;
+  @Override
+  protected HttpResponse setUpHttpResponse(HttpRequest request) {
+    HttpResponseBuilder responseBuilder = HttpResponse.builder();
+    String authHeader = request.getHeaderValue(AUTHORIZATION);
+
+    if (StringUtils.isEmpty(authHeader)) {
+      responseBuilder.statusCode(UNAUTHORIZED.getStatusCode())
+          .addHeader(WWW_AUTHENTICATE, "Basic")
+          .addHeader(WWW_AUTHENTICATE, NTLM.name())
+          .addHeader(WWW_AUTHENTICATE,
+                     DIGEST + " qop=\"auth\",algorithm=MD5-sess," +
+                         "nonce=\"" + NONCE + "\",charset=utf-8,realm=\"INT\"");
+    } else if (authHeader.startsWith(DIGEST) && authHeader.contains(NONCE)) {
+      responseBuilder.statusCode(OK.getStatusCode())
+          .reasonPhrase(OK.getReasonPhrase());
+      choseDigest = true;
+    } else if (authHeader.startsWith(NTLM.name())) {
+      responseBuilder.statusCode(OK.getStatusCode())
+          .reasonPhrase(OK.getReasonPhrase());
+      choseNtlm = true;
+    } else {
+      responseBuilder.statusCode(UNAUTHORIZED.getStatusCode());
     }
+    return responseBuilder.build();
+  }
 
+  public HttpRequestOptions getOptionsDigest() {
+    HttpAuthentication authentication =
+        HttpAuthentication.digest("user", "password").preemptive(false).build();
+    return HttpRequestOptions.builder().authentication(authentication).build();
+  }
 
-    @Test
-    public void httpRequestToMultipleAuthMethodServerSelectsCorrectAuthMethod() throws Exception {
+  public HttpRequestOptions getOptionsNTLM() {
+    HttpAuthentication authentication =
+        HttpAuthentication.ntlm("user", "password").preemptive(false).build();
+    return HttpRequestOptions.builder().authentication(authentication).build();
+  }
 
-        final HttpServer server = getServer(new DefaultServerAddress( InetAddress.getLocalHost(), listenerPort.getNumber()),
-                new ServerIdentifier("context", "name"));
+  public HttpRequest getRequest() {
+    return HttpRequest.builder().method(GET).uri(getUri()).build();
+  }
 
-        server.start();
+  @Test
+  public void onMultipleAuthenticationMethodsSupportedLocallySetMethodIsChosenDigest() {
+    HttpClient client =
+        service.getClientFactory().create(clientBuilder.build());
+    client.start();
+    final Reference<HttpResponse> responseReference = new Reference<>();
+    try {
 
-        HttpRequestPacket requestPacket = mock(HttpRequestPacket.class);
+      HttpRequestOptions options = getOptionsDigest();
+      HttpRequest request = getRequest();
+      client.sendAsync(request, options).whenComplete(
+                                                      (response, exception) -> responseReference.set(response));
 
-        assertFalse(true);
+      pollingProber.check(new ResponseReceivedProbe(responseReference));
+      pollingProber.check(new Probe() {
 
-        //requestPacket.
+        @Override
+        public boolean isSatisfied() {
+          return choseDigest;
+        }
+
+        @Override
+        public String describeFailure() {
+          return "Authorization method did not match locally set method.";
+        }
+      });
+
+    } finally {
+      client.stop();
     }
+  }
+
+  @Test
+  public void onMultipleAuthenticationMethodsSupportedLocallySetMethodIsChosenNTLM() {
+    HttpClient client =
+        service.getClientFactory().create(clientBuilder.build());
+    client.start();
+    final Reference<HttpResponse> responseReference = new Reference<>();
+    try {
+
+      HttpRequestOptions options = getOptionsNTLM();
+      HttpRequest request = getRequest();
+      client.sendAsync(request, options).whenComplete(
+                                                      (response, exception) -> responseReference.set(response));
+
+      pollingProber.check(new ResponseReceivedProbe(responseReference));
+      pollingProber.check(new Probe() {
+
+        @Override
+        public boolean isSatisfied() {
+          return choseNtlm;
+        }
+
+        @Override
+        public String describeFailure() {
+          return "Authorization method did not match locally set method.";
+        }
+      });
+
+    } finally {
+      client.stop();
+    }
+  }
+
 }
