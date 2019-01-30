44ef9d6e40732c353ee477b6c570d9f1b5bf0c98
Maria Mariotti <mmariotti@mmariotti-ltm.internal.salesforce.com>
2019-01-30 11:18:50 -0300

    Refactored test case.

--- src/test/java/org/mule/service/http/impl/service/server/grizzly/HttpRequestMultipleAuthenticationMethodsSupportedTestCase.java
+++ src/test/java/org/mule/service/http/impl/service/server/grizzly/HttpRequestMultipleAuthenticationMethodsSupportedTestCase.java
@@ -6,10 +6,13 @@
  */
 package org.mule.service.http.impl.service.server.grizzly;
 
+import static org.mule.runtime.core.api.util.StringUtils.isEmpty;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
 import static org.mule.service.http.impl.AllureConstants.HttpFeature.HttpStory.CLIENT_AUTHENTICATION;
 import static sun.net.www.protocol.http.AuthScheme.NTLM;
+import static sun.net.www.protocol.http.AuthScheme.BASIC;
+import static sun.net.www.protocol.http.AuthScheme.DIGEST;
 import static org.mule.runtime.http.api.HttpConstants.HttpStatus.UNAUTHORIZED;
 import static org.mule.runtime.http.api.HttpConstants.HttpStatus.OK;
 import static org.mule.runtime.http.api.HttpConstants.Method.GET;
@@ -20,12 +23,11 @@ import static org.mule.service.http.impl.AllureConstants.HttpFeature.HTTP_SERVIC
 import org.junit.After;
 import org.junit.Before;
 import org.mule.runtime.api.util.Reference;
-import org.mule.runtime.core.api.util.StringUtils;
 import org.mule.runtime.http.api.client.HttpClient;
 import org.mule.runtime.http.api.client.HttpClientConfiguration;
 import org.mule.runtime.http.api.client.HttpRequestOptions;
 import org.mule.runtime.http.api.client.auth.HttpAuthentication;
-import org.mule.runtime.http.api.client.auth.HttpAuthenticationBuilder;
+import org.mule.runtime.http.api.client.auth.HttpAuthenticationType;
 import org.mule.runtime.http.api.domain.message.request.HttpRequest;
 import org.mule.runtime.http.api.domain.message.response.HttpResponse;
 import org.mule.runtime.http.api.domain.message.response.HttpResponseBuilder;
@@ -44,58 +46,19 @@ public class HttpRequestMultipleAuthenticationMethodsSupportedTestCase extends A
 
   private static final int TIMEOUT_MILLIS = 1000;
   private static final int POLL_DELAY_MILLIS = 200;
-  private static final String DIGEST = "Digest";
-  private static final String BASIC = "Basic";
   private static final String NONCE =
       "+Upgraded+v1a574e295ff1f41c52582b82815bb734c5c50331c97c4d301bc97f789c5e9e73ca9564b24cbd898ce5f1c13598999faa2ab013ee5b1597087";
-  private static String chosenAuthMethod = "";
-  private static String TEST_USER = "user";
-  private static String TEST_PASS = "password";
-  private static HttpClient client;
+  private static final String TEST_USER = "user";
+  private static final String TEST_PASS = "password";
 
+  private HttpClient client;
+  private String chosenAuthMethod = "";
   private HttpClientConfiguration.Builder clientBuilder = new HttpClientConfiguration.Builder().setName("auth-test");
   private PollingProber pollingProber = new PollingProber(TIMEOUT_MILLIS, POLL_DELAY_MILLIS);
   private final Reference<HttpResponse> responseReference = new Reference<>();
 
   public HttpRequestMultipleAuthenticationMethodsSupportedTestCase(String serviceToLoad) {
     super(serviceToLoad);
-  }
-
-  @Override
-  protected HttpResponse setUpHttpResponse(HttpRequest request) {
-    HttpResponseBuilder responseBuilder = HttpResponse.builder();
-    String authHeader = request.getHeaderValue(AUTHORIZATION);
-    String authType = getRequestAuthType(authHeader);
-    chosenAuthMethod = "";
-
-    if (StringUtils.isEmpty(authHeader)) {
-      responseBuilder.statusCode(UNAUTHORIZED.getStatusCode())
-          .addHeader(WWW_AUTHENTICATE, BASIC)
-          .addHeader(WWW_AUTHENTICATE, NTLM.name())
-          .addHeader(WWW_AUTHENTICATE,
-                     DIGEST + " qop=\"auth\",algorithm=MD5-sess," +
-                         "nonce=\"" + NONCE + "\",charset=utf-8,realm=\"INT\"");
-    } else if (authType.length() > 0) {
-      chosenAuthMethod = authType;
-      responseBuilder.statusCode(OK.getStatusCode())
-          .reasonPhrase(OK.getReasonPhrase());
-    } else {
-      responseBuilder.statusCode(UNAUTHORIZED.getStatusCode());
-    }
-    return responseBuilder.build();
-  }
-
-  public HttpRequestOptions getOptions(HttpAuthenticationBuilder authenticationBuilder) {
-    HttpAuthentication authentication = authenticationBuilder.build();
-    return HttpRequestOptions.builder().authentication(authentication).build();
-  }
-
-  public HttpRequest getRequest() {
-    return HttpRequest.builder().method(GET).uri(getUri()).build();
-  }
-
-  public String getRequestAuthType(String authHeader) {
-    return StringUtils.isEmpty(authHeader) ? "" : authHeader.substring(0, authHeader.indexOf(" "));
   }
 
   @Before
@@ -113,30 +76,76 @@ public class HttpRequestMultipleAuthenticationMethodsSupportedTestCase extends A
 
   @Test
   public void onMultipleAuthenticationMethodsSupportedLocallySetMethodIsChosenDigest() {
-    sendRequestAssertCorrectAuthMethodChosen(HttpAuthentication.digest(TEST_USER, TEST_PASS), DIGEST);
+    sendRequestAssertCorrectAuthMethodChosen(DIGEST.name());
   }
 
   @Test
   public void onMultipleAuthenticationMethodsSupportedLocallySetMethodIsChosenNtlm() {
-    sendRequestAssertCorrectAuthMethodChosen(HttpAuthentication.ntlm(TEST_USER, TEST_PASS), NTLM.name());
+    sendRequestAssertCorrectAuthMethodChosen(NTLM.name());
   }
 
   @Test
   public void onMultipleAuthenticationMethodsSupportedLocallySetMethodIsChosenBasic() {
-    sendRequestAssertCorrectAuthMethodChosen(HttpAuthentication.basic(TEST_USER, TEST_PASS), BASIC);
+    sendRequestAssertCorrectAuthMethodChosen(BASIC.name());
   }
 
-  protected void sendRequestAssertCorrectAuthMethodChosen(HttpAuthenticationBuilder authBuilder, String authMethod) {
-    HttpRequestOptions options = getOptions(authBuilder);
+  @Override
+  protected HttpResponse setUpHttpResponse(HttpRequest request) {
+    HttpResponseBuilder responseBuilder = HttpResponse.builder();
+    String authHeader = request.getHeaderValue(AUTHORIZATION);
+    chosenAuthMethod = getRequestAuthType(authHeader);
+
+    if (isEmpty(authHeader)) {
+      responseBuilder.statusCode(UNAUTHORIZED.getStatusCode())
+          .addHeader(WWW_AUTHENTICATE, BASIC.name())
+          .addHeader(WWW_AUTHENTICATE, NTLM.name())
+          .addHeader(WWW_AUTHENTICATE,
+                     DIGEST.name() + " qop=\"auth\",algorithm=MD5-sess," +
+                         "nonce=\"" + NONCE + "\",charset=utf-8,realm=\"INT\"");
+    } else if (chosenAuthMethod.length() > 0) {
+      responseBuilder.statusCode(OK.getStatusCode())
+          .reasonPhrase(OK.getReasonPhrase());
+    } else {
+      responseBuilder.statusCode(UNAUTHORIZED.getStatusCode());
+    }
+    return responseBuilder.build();
+  }
+
+  protected void sendRequestAssertCorrectAuthMethodChosen(String authMethod) {
+    HttpRequestOptions options = getOptions(authMethod);
     HttpRequest request = getRequest();
     client.sendAsync(request, options).whenComplete(
                                                     (response, exception) -> responseReference.set(response));
     pollingProber.check(new ResponseReceivedProbe(responseReference));
-    pollingProber.check(new ResponseSuccessProbe());
-    assertThat(chosenAuthMethod, equalTo(authMethod));
+    pollingProber.check(new ResponseSuccessProbe(authMethod));
+    assertThat(chosenAuthMethod, equalToIgnoringCase(authMethod));
+  }
+
+  private HttpRequestOptions getOptions(String authMethod) {
+    HttpAuthentication auth = HttpAuthentication.builder()
+        .type(HttpAuthenticationType.valueOf(authMethod))
+        .username(TEST_USER)
+        .password(TEST_PASS)
+        .preemptive(false)
+        .build();
+    return HttpRequestOptions.builder().authentication(auth).build();
+  }
+
+  private HttpRequest getRequest() {
+    return HttpRequest.builder().method(GET).uri(getUri()).build();
+  }
+
+  private String getRequestAuthType(String authHeader) {
+    return isEmpty(authHeader) ? "" : authHeader.substring(0, authHeader.indexOf(" "));
   }
 
   private class ResponseSuccessProbe implements Probe {
+
+    private String authType;
+
+    public ResponseSuccessProbe(String httpAuthType) {
+      authType = httpAuthType;
+    }
 
     @Override
     public boolean isSatisfied() {
@@ -145,7 +154,7 @@ public class HttpRequestMultipleAuthenticationMethodsSupportedTestCase extends A
 
     @Override
     public String describeFailure() {
-      return "Authorization method chosen should be Basic!";
+      return "Authorization method chosen should be " + authType;
     }
   }
 }
