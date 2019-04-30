b8fd426c9af0b0be75810d0d4ed290070e2c5de2
Ezequiel Werner <eze210@gmail.com>
2019-04-30 15:14:34 -0300

    MULE-16756: Now the request streaming is disabled by default
    (cherry picked from commit 2c1dd0cb4c39652aca70136c090e9e3c0c6b4584)

--- src/main/java/org/mule/service/http/impl/service/client/GrizzlyHttpClient.java
+++ src/main/java/org/mule/service/http/impl/service/client/GrizzlyHttpClient.java
@@ -93,8 +93,8 @@ public class GrizzlyHttpClient implements HttpClient {
   private static final int DEFAULT_SEND_AND_DEFER_BUFFER_SIZE = KB.toBytes(10);
   private static final String DEFAULT_DECOMPRESS_PROPERTY_NAME = SYSTEM_PROPERTY_PREFIX + "http.client.decompress";
 
-  private static final String DISABLE_REQUEST_STREAMING_PROPERTY_NAME = SYSTEM_PROPERTY_PREFIX + "http.requestStreaming.disable";
-  private static boolean requestStreamingDisabled = getProperties().containsKey(DISABLE_REQUEST_STREAMING_PROPERTY_NAME);
+  private static final String ENABLE_REQUEST_STREAMING_PROPERTY_NAME = SYSTEM_PROPERTY_PREFIX + "http.requestStreaming.enable";
+  private static boolean requestStreamingEnabled = getProperties().containsKey(ENABLE_REQUEST_STREAMING_PROPERTY_NAME);
 
   private static final int DEFAULT_REQUEST_STREAMING_BUFFER_SIZE = 8 * 1024;
   private static final String REQUEST_STREAMING_BUFFER_LEN_PROPERTY_NAME =
@@ -532,6 +532,6 @@ public class GrizzlyHttpClient implements HttpClient {
   }
 
   private static boolean isRequestStreamingEnabled() {
-    return !requestStreamingDisabled;
+    return requestStreamingEnabled;
   }
 }
