dfe5e66c33810c1c8b3c4c1c1ce9a035b2302e37
fabian.gonzalez <fabian.gonzalez@10.83.49.98>
2020-02-13 12:10:17 -0300

    MULE-18057: Cursors are not properly reset in NTLM dancing

---
+++ src/main/java/org/mule/service/http/impl/service/client/CursorNonBlockingInputStreamFeeder.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.service.http.impl.service.client;
+
+import java.io.IOException;
+
+import org.mule.runtime.api.streaming.bytes.CursorStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ning.http.client.providers.grizzly.FeedableBodyGenerator;
+import com.ning.http.client.providers.grizzly.NonBlockingInputStreamFeeder;
+
+/**
+ * Inputstream feeder used so that we guarantee that a cursor inputstream is properly reset.
+ * 
+ * @since 4.3.0
+ *
+ */
+public class CursorNonBlockingInputStreamFeeder extends NonBlockingInputStreamFeeder {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(CursorNonBlockingInputStreamFeeder.class);
+
+  public CursorNonBlockingInputStreamFeeder(FeedableBodyGenerator feedableBodyGenerator, CursorStream content,
+                                            int internalBufferSize) {
+    super(feedableBodyGenerator, content, internalBufferSize);
+  }
+
+  @Override
+  public void reset() {
+    if (!content.markSupported()) {
+      try {
+        ((CursorStream) content).seek(0);
+      } catch (IOException e) {
+        if (LOGGER.isDebugEnabled()) {
+          LOGGER.debug("Unable to perform seek(0) on cursor stream", e);
+        }
+      }
+    }
+
+    super.reset();
+  }
+
+
+
+}
