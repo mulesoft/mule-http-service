ce87b26376c7ae766b13c010d06e7f96e1e1749e
Fabian Gonzalez <fabian.gonzalez@mulesoft.com>
2020-04-24 17:42:59 -0300

    MULE-18343: Source is not notified of failure on ConnectionException in (#229)

--- src/main/java/org/mule/service/http/impl/service/server/grizzly/ResponseStreamingCompletionHandler.java
+++ src/main/java/org/mule/service/http/impl/service/server/grizzly/ResponseStreamingCompletionHandler.java
@@ -23,7 +23,7 @@ import static org.mule.runtime.http.api.HttpHeaders.Names.CONTENT_LENGTH;
 import static org.mule.service.http.impl.service.server.grizzly.ExecutorPerServerAddressIOStrategy.DELEGATE_WRITES_IN_CONFIGURED_EXECUTOR;
 import static org.slf4j.LoggerFactory.getLogger;
 
-import org.mule.runtime.api.connection.ConnectionException;
+import org.mule.runtime.api.connection.SourceRemoteConnectionException;
 import org.mule.runtime.api.exception.DefaultMuleException;
 import org.mule.runtime.core.api.config.i18n.CoreMessages;
 import org.mule.runtime.http.api.domain.message.response.HttpResponse;
@@ -227,7 +227,7 @@ public class ResponseStreamingCompletionHandler extends BaseResponseCompletionHa
       markConnectionToDelegateWritesInConfiguredExecutor(false);
       close();
       responseStatusCallback.onErrorSendingResponse(ctx.getConnection().isOpen() ? throwable
-          : new ConnectionException(CLIENT_CONNECTION_CLOSED_MESSAGE, throwable));
+          : new SourceRemoteConnectionException(CLIENT_CONNECTION_CLOSED_MESSAGE, throwable));
       resume();
 
     } finally {
