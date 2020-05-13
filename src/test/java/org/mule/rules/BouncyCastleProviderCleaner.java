57afcb830a39c477cbf229f0355167182cf0932c
Mariano Lischetti <lischettim@gmail.com>
2020-05-13 17:39:27 -0300

    MULE 18398: Grizzly SSLFilter at write operation is not validating handshake status before call beginHandshake (#231)
    (cherry picked from commit da90fce6db4bd311ed1945c96d94204fe4cf143b)

---
+++ src/test/java/org/mule/rules/BouncyCastleProviderCleaner.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.rules;
+
+import static java.security.Security.removeProvider;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jsse.provider.BouncyCastleJsseProvider;
+import org.junit.rules.ExternalResource;
+
+public class BouncyCastleProviderCleaner extends ExternalResource {
+
+  private boolean initialized;
+  private Set<String> providers;
+
+  public BouncyCastleProviderCleaner() {
+    Set<String> providers = new HashSet<>();
+    providers.add(BouncyCastleProvider.PROVIDER_NAME);
+    providers.add(BouncyCastleJsseProvider.PROVIDER_NAME);
+    this.providers = providers;
+  }
+
+  @Override
+  protected void before() throws Throwable {
+    if (initialized) {
+      throw new IllegalArgumentException("System property was already initialized");
+    }
+
+    cleanUpProviders();
+
+    initialized = true;
+  }
+
+  protected void cleanUpProviders() {
+    for (String p : providers) {
+      removeProvider(p);
+    }
+  }
+
+  @Override
+  protected void after() {
+    if (!initialized) {
+      throw new IllegalArgumentException("Bouncy castle provider was not initialized");
+    }
+
+    cleanUpProviders();
+
+    initialized = false;
+  }
+
+}
