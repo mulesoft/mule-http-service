a5cffdc40d807b5637a2f948479dd737d9e0d52c
Ezequiel Werner <eze210@gmail.com>
2020-12-03 11:12:21 -0300

    Update parent version and moving test to avoid parametrized

---
+++ src/test/java/org/mule/service/http/impl/functional/client/HttpTransferLengthWithPayloadAllowedTestCase.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.service.http.impl.functional.client;
+
+import static org.mule.service.http.impl.AllureConstants.HttpFeature.HttpStory.TRANSFER_TYPE;
+
+import org.mule.service.http.impl.service.server.grizzly.AbstractHttpTransferLengthTestCase;
+
+import io.qameta.allure.Story;
+
+@Story(TRANSFER_TYPE)
+public class HttpTransferLengthWithPayloadAllowedTestCase extends AbstractHttpTransferLengthTestCase {
+
+  public HttpTransferLengthWithPayloadAllowedTestCase(String serviceToLoad) {
+    super(serviceToLoad, true);
+  }
+}
