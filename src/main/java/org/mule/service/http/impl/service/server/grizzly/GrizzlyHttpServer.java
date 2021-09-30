e92b6ca171c6a792c6e67d69731ed205675582ac
Axel Sanguinetti <asanguinetti@mulesoft.com>
2021-09-30 18:30:13 -0300

    Testing closing remaining connections after graceful shutdown expiration.

--- src/main/java/org/mule/service/http/impl/service/server/grizzly/GrizzlyHttpServer.java
+++ src/main/java/org/mule/service/http/impl/service/server/grizzly/GrizzlyHttpServer.java
@@ -11,12 +11,15 @@ import static java.lang.String.format;
 import static java.lang.System.getProperty;
 import static java.lang.System.nanoTime;
 import static java.lang.Thread.currentThread;
+import static java.util.Collections.synchronizedList;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
 import static org.mule.runtime.api.util.MuleSystemProperties.MULE_LOG_SEPARATION_DISABLED;
 import static org.mule.runtime.core.api.util.ClassUtils.setContextClassLoader;
 import static org.mule.runtime.http.api.server.MethodRequestMatcher.acceptAll;
 import static org.mule.service.http.impl.service.server.grizzly.MuleSslFilter.createSslFilter;
+
+import org.glassfish.grizzly.nio.transport.TCPNIOConnection;
 import org.mule.runtime.api.scheduler.Scheduler;
 import org.mule.runtime.api.tls.TlsContextFactory;
 import org.mule.runtime.http.api.HttpConstants.Protocol;
@@ -31,7 +34,12 @@ import org.mule.runtime.http.api.server.async.HttpResponseReadyCallback;
 import org.mule.service.http.impl.service.server.HttpListenerRegistry;
 
 import java.io.IOException;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.function.Supplier;
 
@@ -64,9 +72,10 @@ public class GrizzlyHttpServer implements HttpServer, Supplier<ExecutorService> 
   private boolean stopping;
   private Supplier<Long> shutdownTimeoutSupplier;
 
-  private volatile int openConnectionsCounter = 0;
-  private final Object openConnectionsSync = new Object();
   private CountAcceptedConnectionsProbe acceptedConnectionsProbe;
+
+  /** List with client connections for cleanup purposes. */
+  private final List<Connection<?>> clientConnections = synchronizedList(new LinkedList<>());
 
   public GrizzlyHttpServer(ServerAddress serverAddress,
                            TCPNIOTransport transport,
@@ -95,8 +104,6 @@ public class GrizzlyHttpServer implements HttpServer, Supplier<ExecutorService> 
       logger.info("Listening for connections on '{}'", listenerUrl());
     }
 
-    openConnectionsCounter = 0;
-
     serverConnection.addCloseListener(new OnCloseConnectionListener());
     stopped = false;
     return this;
@@ -116,17 +123,21 @@ public class GrizzlyHttpServer implements HttpServer, Supplier<ExecutorService> 
       transport.unbind(serverConnection);
 
       if (shutdownTimeout != 0) {
-        synchronized (openConnectionsSync) {
+        synchronized (clientConnections) {
           long remainingMillis = NANOSECONDS.toMillis(stopNanos - nanoTime());
-          while (openConnectionsCounter != 0 && remainingMillis > 0) {
+          while (!clientConnections.isEmpty() && remainingMillis > 0) {
             long millisToWait = min(remainingMillis, 50);
             logger.debug("There are still {} open connections on server stop. Waiting {} milliseconds",
-                         openConnectionsCounter, millisToWait);
-            openConnectionsSync.wait(millisToWait);
+                         clientConnections.size(), millisToWait);
+            clientConnections.wait(millisToWait);
             remainingMillis = NANOSECONDS.toMillis(stopNanos - nanoTime());
           }
-          if (openConnectionsCounter != 0) {
-            logger.warn("There are still {} open connections on server stop.", openConnectionsCounter);
+
+          // graceful shutdown timeout has expired
+          if (!clientConnections.isEmpty()) {
+            // force-closing remaining connections now
+            logger.warn("There are still {} open connections on server stop.", clientConnections.size());
+            clientConnections.forEach(this::tryCloseConnection);
           }
         }
       }
@@ -233,14 +244,14 @@ public class GrizzlyHttpServer implements HttpServer, Supplier<ExecutorService> 
      */
     @Override
     public void onAcceptEvent(Connection serverConnection, Connection clientConnection) {
-      synchronized (openConnectionsSync) {
-        openConnectionsCounter += 1;
-      }
+      clientConnections.add(clientConnection);
       clientConnection.addCloseListener((CloseListener) (closeable, iCloseType) -> {
-        synchronized (openConnectionsSync) {
-          openConnectionsCounter -= 1;
-          if (openConnectionsCounter == 0) {
-            openConnectionsSync.notifyAll();
+        clientConnections.remove(clientConnection);
+        if (clientConnections.isEmpty()) {
+          synchronized (clientConnections) {
+            if (clientConnections.isEmpty()) {
+              clientConnections.notifyAll();
+            }
           }
         }
       });
@@ -264,6 +275,31 @@ public class GrizzlyHttpServer implements HttpServer, Supplier<ExecutorService> 
         closeable.removeCloseListener(this);
         serverConnection.getMonitoringConfig().removeProbes(acceptedConnectionsProbe);
         acceptedConnectionsProbe = null;
+      }
+    }
+  }
+
+  private void tryCloseConnection(Connection<?> acceptedConnection) {
+    if (!(acceptedConnection instanceof TCPNIOConnection)) {
+      if (logger.isWarnEnabled()) {
+        logger.warn("The accepted connection is not an instance of TCPNIOConnection");
+      }
+      return;
+    }
+
+    SelectableChannel selectableChannel = ((TCPNIOConnection) acceptedConnection).getChannel();
+    if (!(selectableChannel instanceof SocketChannel)) {
+      if (logger.isWarnEnabled()) {
+        logger.warn("The accepted connection doesn't hold a SocketChannel");
+      }
+      return;
+    }
+
+    try {
+      ((SocketChannel) selectableChannel).shutdownInput();
+    } catch (IOException e) {
+      if (logger.isWarnEnabled()) {
+        logger.error("There was an exception closing the connection", e);
       }
     }
   }
