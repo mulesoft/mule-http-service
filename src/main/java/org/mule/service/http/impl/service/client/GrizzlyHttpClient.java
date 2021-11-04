9bdd14b95e021cb710e03fa8ac53c01bc58081a4
jcgrethe <jgrethe@itba.edu.ar>
2021-11-04 13:21:52 -0300

    fix comment

--- src/main/java/org/mule/service/http/impl/service/client/GrizzlyHttpClient.java
+++ src/main/java/org/mule/service/http/impl/service/client/GrizzlyHttpClient.java
@@ -130,7 +130,7 @@ public class GrizzlyHttpClient implements HttpClient {
   public static final String CUSTOM_MAX_HTTP_PACKET_HEADER_SIZE = SYSTEM_PROPERTY_PREFIX + "http.client.headerSectionSize";
 
   // By default, we will delegate the redirect to Grizzly. If the system property is set to true Mule will handle redirects requests.
-  // Currently, if Grizzly will fail to handle concurrent redirects if streaming is not enabled.
+  // Currently, Grizzly will fail to handle concurrent redirects if streaming is not enabled.
   private static final String ENABLE_MULE_REDIRECT_PROPERTY_NAME = SYSTEM_PROPERTY_PREFIX + "http.enableMuleRedirect";
   private static final boolean enableMuleRedirect = parseBoolean(getProperty(ENABLE_MULE_REDIRECT_PROPERTY_NAME, "false"));
 
