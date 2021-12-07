b1fb04e2fc3668ee67a11f96a5020a7e4277ea9c
sofiamorseletto <sofimorseletto@gmail.com>
2021-12-07 11:36:31 -0300

    MULE-19837: Grizzly Max Number of Headers (#422)
    * Add option to set the max num of request and response headers supported
    
    (cherry picked from commit ba773e621c9a542852e907ec07454257e9932752)
    
    * MULE-19837: Grizzly Max Number of Headers
    
    (cherry picked from commit 3f90ab3c92efdfaa487342eb0884ae2ad56f83dd)
    
    * Fix
    
    (cherry picked from commit 54d5b1ad2f9362e7bfc3c2fa83c83d3b2b5d63bc)
    
    * Fixed PR comments
    
    (cherry picked from commit 41d6992eb8fa59015fb3a06c552bc6e7001ced39)
    
    * Fixed response system property assignation
    
    (cherry picked from commit 03c7daadbc74c8b37669db7af544621563047aa7)
    
    * Fix
    
    (cherry picked from commit 80840b72838e835e6e863eff6158a90b95089ff5)
    
    * Fix
    
    (cherry picked from commit 8d63375c31971c42d6b34402e7bdbd3d03d8bd38)
    
    * Fixed format
    
    * Adding max client request headers
    
    * Fixes
    
    * Adding license
    
    * Fixed tests
    
    Co-authored-by: panizzag <gaston.panizza@gmail.com>
    (cherry picked from commit 3a342ea1d2f62d3eb21dabe623a58f6ec3f4d8a7)

--- src/test/java/org/mule/service/http/impl/service/server/grizzly/HttpGrizzlyServerManagerTestCase.java
+++ src/test/java/org/mule/service/http/impl/service/server/grizzly/HttpGrizzlyServerManagerTestCase.java
@@ -6,20 +6,25 @@
  */
 package org.mule.service.http.impl.service.server.grizzly;
 
+import static java.lang.Integer.parseInt;
 import static java.lang.Runtime.getRuntime;
+import static java.util.concurrent.Executors.newCachedThreadPool;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.glassfish.grizzly.http.util.MimeHeaders.MAX_NUM_HEADERS_DEFAULT;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mule.runtime.api.util.MuleSystemProperties.SYSTEM_PROPERTY_PREFIX;
 import static org.mule.runtime.http.api.HttpConstants.ALL_INTERFACES_ADDRESS;
 import static org.mule.runtime.http.api.HttpConstants.Protocol.HTTP;
 import static org.mule.runtime.http.api.HttpConstants.Protocol.HTTPS;
 import static org.mule.service.http.impl.AllureConstants.HttpFeature.HTTP_SERVICE;
 import static org.mule.service.http.impl.AllureConstants.HttpFeature.HttpStory.SERVER_MANAGEMENT;
+import static org.mule.tck.junit4.rule.SystemProperty.callWithProperty;
 
 import org.mule.runtime.api.tls.TlsContextFactory;
 import org.mule.runtime.http.api.server.HttpServer;
@@ -28,6 +33,7 @@ import org.mule.runtime.http.api.server.ServerCreationException;
 import org.mule.runtime.http.api.tcp.TcpServerSocketProperties;
 import org.mule.service.http.impl.service.server.DefaultServerAddress;
 import org.mule.service.http.impl.service.server.HttpListenerRegistry;
+import org.mule.service.http.impl.service.server.ServerAddressMap;
 import org.mule.service.http.impl.service.server.ServerIdentifier;
 
 import java.io.IOException;
@@ -37,6 +43,7 @@ import java.util.concurrent.ExecutorService;
 import io.qameta.allure.Description;
 import io.qameta.allure.Issue;
 import org.glassfish.grizzly.nio.transport.TCPNIOTransport;
+import org.glassfish.grizzly.http.HttpServerFilter;
 import org.junit.Test;
 
 import io.qameta.allure.Feature;
@@ -144,6 +151,88 @@ public class HttpGrizzlyServerManagerTestCase extends AbstractGrizzlyServerManag
            times(removes)).removeExecutor(any());
   }
 
+  @Issue("MULE-19837")
+  @Description("When the max number of request and response headers are NOT set by System Properties, they should be " +
+      "assigned correctly by default. We check that the variables are properly set because we are delegating the max headers" +
+      " amount check to Grizzly")
+  @Test
+  public void testMaxRequestAndResponseHeadersIfNotSetBySystemPropertyAreSetByDefault()
+      throws Throwable {
+    GrizzlyServerManager.refreshSystemProperties();
+    HttpServerFilter httpServerFilter = getHttpServerFilter(serverManager);
+
+    assertThat(getMaxHeaders(httpServerFilter, "maxRequestHeaders"), is(MAX_NUM_HEADERS_DEFAULT));
+    assertThat(getMaxHeaders(httpServerFilter, "maxResponseHeaders"), is(MAX_NUM_HEADERS_DEFAULT));
+  }
+
+  @Issue("MULE-19837")
+  @Description("When the max number of request headers are set by System Properties, they should be " +
+      "assigned correctly. We check that the variables are properly set because we are delegating the max headers" +
+      " amount check to Grizzly")
+  @Test
+  public void testMaxRequestHeadersCanBeSetBySystemProperty()
+      throws Throwable {
+    String maxSetRequestHeaders = "80";
+
+    GrizzlyServerManager grizzlyServerManager =
+        callWithProperty(SYSTEM_PROPERTY_PREFIX + "http.MAX_SERVER_REQUEST_HEADERS", maxSetRequestHeaders,
+                         this::refreshSystemPropertiesAndCreateServerManager);
+    HttpServerFilter httpServerFilter = getHttpServerFilter(grizzlyServerManager);
+
+    assertThat(getMaxHeaders(httpServerFilter, "maxRequestHeaders"), is(parseInt(maxSetRequestHeaders)));
+  }
+
+  @Issue("MULE-19837")
+  @Description("When the max number of response headers are set by System Properties, they should be " +
+      "assigned correctly. We check that the variables are properly set because we are delegating the max headers" +
+      " amount check to Grizzly")
+  @Test
+  public void testMaxResponseHeadersCanBeSetBySystemProperty()
+      throws Throwable {
+    String maxSetResponseHeaders = "70";
+
+    GrizzlyServerManager grizzlyServerManager =
+        callWithProperty(SYSTEM_PROPERTY_PREFIX + "http.MAX_SERVER_RESPONSE_HEADERS", maxSetResponseHeaders,
+                         this::refreshSystemPropertiesAndCreateServerManager);
+    HttpServerFilter httpServerFilter = getHttpServerFilter(grizzlyServerManager);
+
+    assertThat(getMaxHeaders(httpServerFilter, "maxResponseHeaders"), is(parseInt(maxSetResponseHeaders)));
+  }
+
+  private GrizzlyServerManager refreshSystemPropertiesAndCreateServerManager() {
+    GrizzlyServerManager.refreshSystemProperties();
+    return new GrizzlyServerManager(newCachedThreadPool(), newCachedThreadPool(),
+                                    newCachedThreadPool(), new HttpListenerRegistry(),
+                                    new DefaultTcpServerSocketProperties(),
+                                    getRuntime().availableProcessors());
+  }
+
+  private HttpServerFilter getHttpServerFilter(GrizzlyServerManager grizzlyServerManager) throws Throwable {
+    ServerAddress serverAddress = new DefaultServerAddress(ALL_INTERFACES_ADDRESS, listenerPort.getNumber());
+
+    grizzlyServerManager.createServerFor(serverAddress, () -> muleContext.getSchedulerService().ioScheduler(), true,
+                                         (int) SECONDS.toMillis(DEFAULT_TEST_TIMEOUT_SECS),
+                                         new ServerIdentifier("context", "name"),
+                                         () -> muleContext.getConfiguration().getShutdownTimeout());
+
+    Field httpServerFilterDelegate = GrizzlyServerManager.class.getDeclaredField("httpServerFilterDelegate");
+    httpServerFilterDelegate.setAccessible(true);
+    GrizzlyAddressDelegateFilter grizzlyAddressDelegateFilter =
+        (GrizzlyAddressDelegateFilter) httpServerFilterDelegate.get(grizzlyServerManager);
+
+    Field filters = GrizzlyAddressDelegateFilter.class.getDeclaredField("filters");
+    filters.setAccessible(true);
+    ServerAddressMap serverAddressMap = (ServerAddressMap) filters.get(grizzlyAddressDelegateFilter);
+    return (HttpServerFilter) serverAddressMap.get(serverAddress);
+  }
+
+  private int getMaxHeaders(HttpServerFilter httpServerFilter, String maxHeaders)
+      throws NoSuchFieldException, IllegalAccessException {
+    Field maxRequestHeadersField = HttpServerFilter.class.getDeclaredField(maxHeaders);
+    maxRequestHeadersField.setAccessible(true);
+    return (int) maxRequestHeadersField.get(httpServerFilter);
+  }
+
   @Override
   protected HttpGrizzlyServerManagerTestDecorator createServerManager(HttpListenerRegistry registry,
                                                                       DefaultTcpServerSocketProperties socketProperties) {
@@ -152,6 +241,7 @@ public class HttpGrizzlyServerManagerTestCase extends AbstractGrizzlyServerManag
   }
 
   private static class HttpGrizzlyServerManagerTestDecorator extends GrizzlyServerManager {
+
 
     WorkManagerSourceExecutorProvider spiedExecutorProvider;
 
