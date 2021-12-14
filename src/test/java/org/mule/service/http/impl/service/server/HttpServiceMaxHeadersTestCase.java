a309351033f6d8e68d3435ca33d407e91165e8a3
sofiamorseletto <sofimorseletto@gmail.com>
2021-12-14 09:56:15 -0300

    MULE-19837: Grizzly Max Number of Headers  (#429)
    * MULE-19837: Grizzly Max Number of Headers (#422)
    
    * Add option to set the max num of request and response headers supported
    
    (cherry picked from commit ba773e621c9a542852e907ec07454257e9932752)
    
    * MULE-19837: Grizzly Max Number of Headers
    
    (cherry picked from commit 3f90ab3c92efdfaa487342eb0884ae2ad56f83dd)
    
    * Fix
    
    (cherry picked from commit 54d5b1ad2f9362e7bfc3c2fa83c83d3b2b5d63bc)
    
    * Fixed PR comments
    
    (cherry picked from commit 41d6992eb8fa59015fb3a06c552bc6e7001ced39)
    
    * Fixed response system property assignation
    
    (cherry picked from commit 03c7daadbc74c8b37669db7af544621563047aa7)
    
    * Fix
    
    (cherry picked from commit 80840b72838e835e6e863eff6158a90b95089ff5)
    
    * Fix
    
    (cherry picked from commit 8d63375c31971c42d6b34402e7bdbd3d03d8bd38)
    
    * Fixed format
    
    * Adding max client request headers
    
    * Fixes
    
    * Adding license
    
    * Fixed tests
    
    Co-authored-by: panizzag <gaston.panizza@gmail.com>
    (cherry picked from commit 3a342ea1d2f62d3eb21dabe623a58f6ec3f4d8a7)
    
    * MULE-19837: Update Grizzly AHC version to 1.14-MULE-021 (#426)
    
    (cherry picked from commit 85ac581c0d641e9bc6b6ad4cab63719c598df339)
    
    * Fix
    
    * MULE-19837: Fixing tests (#432)
    
    * MULE-19837: Grizzly Max Number of Headers
    
    (cherry picked from commit 3f90ab3c92efdfaa487342eb0884ae2ad56f83dd)
    
    * Fix
    
    (cherry picked from commit 54d5b1ad2f9362e7bfc3c2fa83c83d3b2b5d63bc)
    
    * Adding max client request headers
    
    * Fixes
    
    * Added teardown to tests
    
    * Fix
    
    * Formatting
    
    (cherry picked from commit a9a5c44b93d32f5b6c72faf159a9fcc76a8bedce)
    (cherry picked from commit f78ad7edd2a8d387ee028edfacb0cc9e6760f750)

---
+++ src/test/java/org/mule/service/http/impl/service/server/HttpServiceMaxHeadersTestCase.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.service.http.impl.service.server;
+
+import static java.lang.String.format;
+import static java.util.Collections.singletonList;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mule.runtime.api.metadata.MediaType.TEXT;
+import static org.mule.runtime.api.util.MuleSystemProperties.SYSTEM_PROPERTY_PREFIX;
+import static org.mule.runtime.http.api.HttpConstants.HttpStatus.REQUEST_TOO_LONG;
+import static org.mule.runtime.http.api.HttpConstants.Method.GET;
+import static org.mule.runtime.http.api.HttpHeaders.Names.CONTENT_TYPE;
+import static org.mule.tck.junit4.rule.SystemProperty.callWithProperty;
+
+import org.mule.runtime.http.api.HttpConstants;
+import org.mule.runtime.http.api.domain.entity.ByteArrayHttpEntity;
+import org.mule.runtime.http.api.domain.message.response.HttpResponse;
+import org.mule.runtime.http.api.server.HttpServer;
+import org.mule.runtime.http.api.server.HttpServerConfiguration;
+import org.mule.service.http.impl.functional.server.AbstractHttpServerTestCase;
+import org.mule.service.http.impl.service.server.grizzly.GrizzlyServerManager;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
+import org.apache.http.NoHttpResponseException;
+import org.apache.http.StatusLine;
+import org.apache.http.client.fluent.Request;
+import org.junit.After;
+import org.junit.Test;
+
+public class HttpServiceMaxHeadersTestCase extends AbstractHttpServerTestCase {
+
+  private static final String SIMPLE_ENDPOINT = "test";
+  private static final String PAYLOAD1 = "p1";
+
+  public HttpServiceMaxHeadersTestCase(String serviceToLoad) {
+    super(serviceToLoad);
+  }
+
+  @Override
+  protected String getServerName() {
+    return "max-headers-test";
+  }
+
+  private void registerHandler(HttpConstants.Method httpMethod, String endpoint, String payload, HttpServer httpServer) {
+    httpServer.addRequestHandler(singletonList(httpMethod.name()), "/" + endpoint, (requestContext, responseCallback) -> {
+      responseCallback.responseReady(HttpResponse.builder().entity(new ByteArrayHttpEntity(payload.getBytes()))
+          .addHeader(CONTENT_TYPE, TEXT.toRfcString())
+          .build(), new IgnoreResponseStatusCallback());
+    });
+  }
+
+  @After
+  public void tearDown() {
+    GrizzlyServerManager.refreshSystemProperties();
+  }
+
+  @Issue("MULE-19837")
+  @Description("When the max number of request headers are set by System Properties, they should be " +
+      "assigned correctly. If this max number is exceeded, a 413 status code should be returned.")
+  @Test
+  public void whenRequestHasMoreHeadersThanMaxNumberThen413ShouldBeReturned() throws Throwable {
+    HttpServer httpServer = callWithProperty(SYSTEM_PROPERTY_PREFIX + "http.MAX_SERVER_REQUEST_HEADERS", "3",
+                                             this::refreshSystemPropertiesAndCreateServer);
+    registerHandler(GET, SIMPLE_ENDPOINT, PAYLOAD1, httpServer);
+
+    Request request =
+        Request.Get(format("http://%s:%s/%s", httpServer.getServerAddress().getIp(), port.getValue(), SIMPLE_ENDPOINT));
+    request.addHeader("header1", "someValue");
+    request.addHeader("header2", "someValue");
+    request.addHeader("header3", "someValue");
+    request.addHeader("header4", "someValue");
+
+    org.apache.http.HttpResponse response = request.execute().returnResponse();
+    StatusLine statusLine = response.getStatusLine();
+
+    assertThat(statusLine.getStatusCode(), is(REQUEST_TOO_LONG.getStatusCode()));
+
+    httpServer.stop();
+    httpServer.dispose();
+  }
+
+  @Issue("MULE-19837")
+  @Description("When the max number of response headers are set by System Properties, they should be " +
+      "assigned correctly. If this max number is exceeded, a NoHttpResponseException should be thrown")
+  @Test(expected = NoHttpResponseException.class)
+  public void whenResponseHasMoreHeadersThanMaxNumberThenExceptionShouldBeThrown() throws Throwable {
+    HttpServer httpServer = callWithProperty(SYSTEM_PROPERTY_PREFIX + "http.MAX_SERVER_RESPONSE_HEADERS", "2",
+                                             this::refreshSystemPropertiesAndCreateServer);
+    registerHandler(GET, SIMPLE_ENDPOINT, PAYLOAD1, httpServer);
+    Request request =
+        Request.Get(format("http://%s:%s/%s", httpServer.getServerAddress().getIp(), port.getValue(), SIMPLE_ENDPOINT));
+    try {
+      request.execute();
+    } finally {
+      httpServer.stop();
+      httpServer.dispose();
+    }
+  }
+
+  private HttpServer refreshSystemPropertiesAndCreateServer() throws Exception {
+    GrizzlyServerManager.refreshSystemProperties();
+    HttpServer httpServer = service.getServerFactory().create(configureServer(new HttpServerConfiguration.Builder()
+        .setHost("localhost")
+        .setPort(port.getNumber())
+        .setName(getServerName()))
+            .build());
+    httpServer.start();
+    return httpServer;
+  }
+}
