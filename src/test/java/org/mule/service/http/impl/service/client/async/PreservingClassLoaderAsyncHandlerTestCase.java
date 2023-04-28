1c6426cabab47091a1091a55e88de5b0aad775b1
Hyeran Park <53480609+hyerans@users.noreply.github.com>
2023-04-27 23:17:14 -0700

    W-10620466: Remove PowerMock and switch to mockito 4.11.0 in mule-http-service (#510)
    * W-10620466: Remove PowerMock and switch to mockito 4.x+ in http-service
    
    * fix PreservingClassLoaderAsyncHandlerTestCase

--- src/test/java/org/mule/service/http/impl/service/client/async/PreservingClassLoaderAsyncHandlerTestCase.java
+++ src/test/java/org/mule/service/http/impl/service/client/async/PreservingClassLoaderAsyncHandlerTestCase.java
@@ -9,10 +9,11 @@ package org.mule.service.http.impl.service.client.async;
 import static java.lang.Thread.currentThread;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.when;
+import static org.mockito.junit.MockitoJUnit.rule;
+
 import static org.mule.runtime.core.api.util.ClassUtils.withContextClassLoader;
 import org.mule.runtime.api.util.Reference;
 import org.mule.tck.junit4.AbstractMuleTestCase;
@@ -20,13 +21,15 @@ import org.mule.tck.junit4.AbstractMuleTestCase;
 import com.ning.http.client.AsyncHandler;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
 
-@RunWith(MockitoJUnitRunner.class)
 public class PreservingClassLoaderAsyncHandlerTestCase extends AbstractMuleTestCase {
+
+  @Rule
+  public MockitoRule rule = rule();
 
   @Mock
   private AsyncHandler<Integer> delegate;
@@ -48,7 +51,7 @@ public class PreservingClassLoaderAsyncHandlerTestCase extends AbstractMuleTestC
     Reference<ClassLoader> classLoaderOnCompleted = new Reference<>();
     when(delegate.onCompleted()).then(invocation -> {
       classLoaderOnCompleted.set(currentThread().getContextClassLoader());
-      return doCallRealMethod();
+      return "completed";
     });
 
     // Call the method with other classloader
@@ -61,7 +64,7 @@ public class PreservingClassLoaderAsyncHandlerTestCase extends AbstractMuleTestC
     Reference<ClassLoader> classLoaderOnThrowable = new Reference<>();
     doAnswer(invocation -> {
       classLoaderOnThrowable.set(currentThread().getContextClassLoader());
-      return doCallRealMethod();
+      return "completed";
     }).when(delegate).onThrowable(any(Throwable.class));
 
     // Call the method with other classloader
