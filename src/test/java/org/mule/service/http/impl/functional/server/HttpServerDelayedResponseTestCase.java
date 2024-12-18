a0e6d2fef1bdfadd1f54894d0f0c57a4dbd6e178
Ezequiel Werner <eze210@gmail.com>
2024-12-18 11:50:02 -0300

    W-17370109: Add comments for the next time somebody sees that implementation (#701)

--- src/test/java/org/mule/service/http/impl/functional/server/HttpServerDelayedResponseTestCase.java
+++ src/test/java/org/mule/service/http/impl/functional/server/HttpServerDelayedResponseTestCase.java
@@ -8,6 +8,8 @@ package org.mule.service.http.impl.functional.server;
 
 import static org.mule.runtime.core.api.config.MuleProperties.MULE_HOME_DIRECTORY_PROPERTY;
 import static org.mule.runtime.http.api.HttpConstants.HttpStatus.OK;
+import static org.mule.service.http.impl.AllureConstants.HttpFeature.HTTP_SERVICE;
+import static org.mule.service.http.impl.AllureConstants.HttpFeature.HttpStory.SSE;
 import static org.mule.service.http.impl.config.ContainerTcpServerSocketProperties.PROPERTY_PREFIX;
 
 import static java.lang.Thread.sleep;
@@ -38,6 +40,8 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Writer;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -49,9 +53,16 @@ import org.junit.rules.TemporaryFolder;
 
 /**
  * This test validates that a server response can be delayed as long as needed by deferring its body writing. As it is tested, it
- * also validates that a streaming enabled client can handle such response by first analyzing its metadata and then awaiting for
+ * also validates that a streaming enabled client can handle such response by first analyzing its metadata and then waiting for
  * the body parts.
+ *
+ * <p>
+ * Archeology comment: This behavior was added to support Server Side Events (SSE). The implementation sends a response with an
+ * "infinite" content-length (not chunked), and closes the connection once all the data was sent. A better technology to implement
+ * this use-case is WebSockets.
  */
+@Feature(HTTP_SERVICE)
+@Story(SSE)
 public class HttpServerDelayedResponseTestCase extends AbstractHttpServerTestCase {
 
   private static final String INITIAL_CHUNK = ":";
